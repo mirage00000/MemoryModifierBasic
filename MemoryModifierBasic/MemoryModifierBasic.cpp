50ee024da0349d047b3135841564cc3aaaaadbdc
mirage00000 <150505621+mirage00000@users.noreply.github.com>
2023-11-11 17:52:12 -0500

    cleanup and search script deletion

--- MemoryModifierBasic/MemoryModifierBasic.cpp
+++ MemoryModifierBasic/MemoryModifierBasic.cpp
@@ -1,64 +1,62 @@
 #include <iostream>
 #include <Windows.h>
-#include <vector>
 #include <TlHelp32.h>
-#include "findaddress.h"
-#include <sstream>
-#include <iomanip>
+#include <string>
+
+struct Data {
+    int intValue;
+    float floatValue;
+};
 
 int main() {
-    printf("Welcome to the memory modification tool!\n");
-    std::string winTitl;
-    std::string procName;
-    std::string modAddress;
-    int bytesToMod;
+    std::cout << "Welcome to the memory modification tool!\n";
+
+    std::string winTitle;
     std::cout << "Enter the window title: ";
-    std::cin >> winTitl;
-    std::cout << "Enter the process name: ";
-    std::cin >> procName;
-    findaddress(procName);
-    int bufferSize = MultiByteToWideChar(CP_UTF8, 0, winTitl.c_str(), -1, nullptr, 0);
+    std::getline(std::cin, winTitle);
 
+    int bufferSize = MultiByteToWideChar(CP_UTF8, 0, winTitle.c_str(), -1, nullptr, 0);
     wchar_t* wideStr = new wchar_t[bufferSize];
-    MultiByteToWideChar(CP_UTF8, 0, winTitl.c_str(), -1, wideStr, bufferSize);
+    MultiByteToWideChar(CP_UTF8, 0, winTitle.c_str(), -1, wideStr, bufferSize);
+    LPCWSTR winTitleWide = wideStr;
 
-    LPCWSTR     winTitle = wideStr;
-    printf("\n");
+    std::string modAddress;
     std::cout << "Enter the address: ";
     std::cin >> modAddress;
     unsigned long ulModAddress = std::stoul(modAddress, nullptr, 16);
-    std::cout << "Enter the bytes to modify: ";
-    std::cin >> bytesToMod;
 
-    HWND hWnd = FindWindow(0, winTitle);
+    int bytesToMod;
+    std::cout << "Enter the bytes to modify: ";
+    std::cin >> std::hex >> bytesToMod;
+
+    HWND hWnd = FindWindow(0, winTitleWide);
     if (hWnd == NULL) {
-        MessageBox(0, L"Error! The script wasn't able to find a corresponding PID for the window. Make sure you specified the title correctly.", L"Error!", MB_OK | MB_ICONERROR); // Use | instead of +
+        MessageBox(0, L"Error! The script couldn't find a corresponding PID for the window. Make sure you specified the title correctly.", L"Error!", MB_OK | MB_ICONERROR);
     }
     else {
-        DWORD proc_id;
-        GetWindowThreadProcessId(hWnd, &proc_id);
-        HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, proc_id);
+        DWORD procId;
+        GetWindowThreadProcessId(hWnd, &procId);
+        HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, procId);
         if (!hProcess) {
             MessageBox(NULL, L"Cannot open process!", L"Error!", MB_OK | MB_ICONERROR);
         }
         else {
-            union Data {
-                int intValue;
-                float floatValue;
-                // Add other data types if needed
-            };
             Data data;
             data.intValue = bytesToMod;
-            SIZE_T dataSize = sizeof(data);
+
+            std::cout << "You entered: " << bytesToMod << std::endl;
+
             if (WriteProcessMemory(hProcess, reinterpret_cast<LPVOID>(ulModAddress), &data, sizeof(data), NULL)) {
-                MessageBox(NULL, L"Done! Memory written succesfully.", L"Success!", MB_OK | MB_ICONINFORMATION);
+                MessageBox(NULL, L"Done! Memory written successfully.", L"Success!", MB_OK | MB_ICONINFORMATION);
             }
             else {
-                MessageBox(NULL, L"Error! Cannot write the memory.", L"Error!", MB_OK | MB_ICONERROR);
+                MessageBox(NULL, L"Error! Cannot write to memory.", L"Error!", MB_OK | MB_ICONERROR);
             }
+
             CloseHandle(hProcess);
         }
     }
 
+    delete[] wideStr;
     return 0;
 }
--- MemoryModifierBasic/findaddress.h
+++
@@ -1,6 +0,0 @@
-#ifndef FINDADDRESS_H
-#define FINDADDRESS_H
-
-int findaddress(const std::string& str);
-
-#endif
